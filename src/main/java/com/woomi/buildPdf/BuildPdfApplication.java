9919c0244bf0f602bf064019d2c3be576b09f751
Arif <ogbonearif@gmail.com>
2022-04-06 22:07:58 +0000

    Initial commit

---
+++ src/main/java/com/woomi/buildPdf/BuildPdfApplication.java
@@ -0,0 +1,13 @@
+package com.woomi.buildPdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildPdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildPdfApplication.class, args);
+	}
+
+}
